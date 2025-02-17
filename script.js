0eaa1edfbb850b97899c3648b664bcd6ddbdee14
Jevon Jajonie <jevonjaj@gmail.com>
2025-02-17 13:44:37 -0500

    Add collapsible buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var _this = this;
 var changeColor = function (button) {
     button.className = button.className === "stack button" ? "stack success" : "stack button";
 };
@@ -5,3 +6,16 @@ var autoResize = function (textArea) {
     textArea.style.height = "auto";
     textArea.style.height = textArea.scrollHeight + "px";
 };
+var buttons = document.querySelectorAll('.button');
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        _this.classList.toggle("active");
+        var content = _this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        }
+        else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+});
--- script.ts
+++ script.ts
@@ -6,3 +6,18 @@ const autoResize = (textArea: HTMLTextAreaElement) => {
 	textArea.style.height = "auto";
 	textArea.style.height = textArea.scrollHeight + "px";
 }
+
+const buttons = document.querySelectorAll('.button') as NodeListOf<HTMLElement>;
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
+		}
+		else {
+			content.style.maxHeight = content.scrollHeight + "px"
+		}
+	});
+});
